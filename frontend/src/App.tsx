bf15fb09e2484371ed087cbd4a6855dcb8af0fb2
Dimas Bintang <dimasbintang1818@gmail.com>
2026-01-16 15:25:47 +0700

    feat : add manajemen and dashboard routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,10 @@ const HomePage = lazy(() => import("./homepage/page"));
 const LoginPage = lazy(() => import("./login/page"));
 const RegisterPage = lazy(() => import("./register/page"));
 const ProductPage = lazy(() => import("./products/page"));
-const DashboardPage = lazy(() => import("./dashboard/page"));
+const DashboardPage = lazy(() => import("./admin/dashboard/page"));
+const ManajemenProductsPage = lazy(
+  () => import("./admin/manajemen-products/page")
+);
 
 function App() {
   return (
@@ -24,7 +27,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/products" element={<ProductPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/admin/dashboard" element={<DashboardPage />} />
+        <Route path="/admin/products" element={<ManajemenProductsPage />} />
       </Routes>
     </Suspense>
   );
