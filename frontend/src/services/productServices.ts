f40eef3be46f44017d1f6aa63f8f787eb567f056
Dimas Bintang <dimasbintang1818@gmail.com>
2026-01-16 16:15:21 +0700

    refactoring wrong url, and wrong method also create params to for indexing

--- frontend/src/services/productServices.ts
+++ frontend/src/services/productServices.ts
@@ -1,29 +1,43 @@
 import api from "../lib/api";
 import { type Courses, type CreateCoursesDTO } from "@/types/courses";
+import { type AxiosResponse } from "axios";
+
+const ENDPOINT = "/courses";
 
 export const productService = {
   // Ambil semua data dengan dukungan filter
-  getAll: async (category?: string, page?: number, limit?: number) => {
-    const response = await api.get<Courses[]>("/courses", {
-      params: { category, _page: page, _limit: limit }, // akan menjadi /posts?author=Budi
-    });
+  getAll: async (category?: string, page: number = 1, limit?: number) => {
+    const params: any = { category, select: "*", order: "id.asc" };
+
+    // Convert page/limit to PostgREST offset/limit
+    if (limit) {
+      params.limit = limit;
+      params.offset = (page - 1) * limit;
+    }
+
+    const response = await api.get<Courses[]>(ENDPOINT, { params });
+    console.log(response.data);
     return response.data;
   },
 
   // Create data baru
-  create: async (data: CreateCoursesDTO) => {
-    const response = await api.post<Courses>("/courses", data);
+  create: async (data: Partial<CreateCoursesDTO>) => {
+    const response = await api.post(ENDPOINT, data);
     return response.data;
   },
 
   // Update data (PUT mengganti seluruh objek, PATCH hanya sebagian)
-  update: async (id: string, data: Partial<CreateCoursesDTO>) => {
-    const response = await api.patch<Courses>(`/courses/${id}`, data);
-    return response.data;
+  update: async (id: number, data: Partial<CreateCoursesDTO>) => {
+    const response: AxiosResponse<Courses> = await api.patch(
+      `${ENDPOINT}?id=eq.${id}`,
+      data
+    );
+
+    return response.status;
   },
 
   // Delete data
-  delete: async (id: string) => {
-    await api.delete(`/courses/${id}`);
+  delete: async (id: number) => {
+    await api.delete(`${ENDPOINT}?id=eq.${id}`);
   },
 };
