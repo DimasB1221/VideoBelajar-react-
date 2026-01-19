41972ff166ecc91f4475686b0b54776778597b8c
Dimas Bintang <dimasbintang1818@gmail.com>
2026-01-19 12:10:22 +0700

    refactoring : refactoring messy class tailwind code

--- frontend/src/homepage/page.tsx
+++ frontend/src/homepage/page.tsx
@@ -1,15 +1,13 @@
 import { Navbar01 } from "@/components/ui/shadcn-io/navbar-01";
-import hero from "../../public/hero.jpg";
 import { Button } from "@/components/ui/button";
 import Card from "@/homepage/card";
-
-import newsletter from "../../public/newsletter.jpg";
 import Footer from "@/components/ui/footer";
 import { productService } from "@/services/productServices";
 import { useState, useEffect } from "react";
 import { type Courses } from "@/types/courses";
 
 function HomePage() {
+  // Fetch products (courses)
   const [products, setProducts] = useState<Courses[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string>("");
@@ -40,19 +38,17 @@ function HomePage() {
       {/* main section */}
       <main className="bg-[#FFFDF3] w-[100%]">
         {/* hero section */}
-        <section className="hero text-white mx-auto w-[94vw] relative mt-3 lg:w-[90vw] xl:w-[80vw]">
-          <img
-            src={hero}
-            alt=""
-            className="rounded-md absolute inset-0 top-2 h-[335px] w-[94vw] lg:w-[90vw] xl:w-[80vw]"
-          />
-          <section className="overlay absolute top-2 text-center bg-black/80 rounded-md h-[335px] flex flex-col justify-center items-center w-[94vw] lg:w-[90vw] xl:w-[80vw]">
-            <section className="hero-text flex flex-col justify-center items-center gap-4">
+        {/* responsive width juga custom class bisa diliat di index.css */}
+        <section className="text-white mx-auto relative mt-3 responsive-width container">
+          <div className="hero-bg rounded-md absolute top-2 h-[335px] w-full"></div>
+          <section className="overlay-style w-full">
+            {/* Flex-center adalah custom style bisa diliat di index.css */}
+            <section className="hero-text flex-col flex-center gap-4">
               <h1 className="font-bold">
                 Revolusi Pembelajaran: Temukan Ilmu Baru melalui Platform Video
                 Interaktif!
               </h1>
-              <p className="w-[90vw]">
+              <p className="text-center">
                 Temukan ilmu baru yang menarik dan mendalam melalui koleksi
                 video pembelajaran berkualitas tinggi. Tidak hanya itu, Anda
                 juga dapat berpartisipasi dalam latihan interaktif yang akan
@@ -65,40 +61,30 @@ function HomePage() {
           </section>
         </section>
         {/* information section */}
-        <section className="information mx-auto relative top-90 w-[94vw] lg:w-[90vw] xl:w-[80vw]">
+        <section className="information mx-auto relative top-90 responsive-width container">
           <h1 className="text-[24px] font-bold">
             Koleksi Video <br /> Pembelajaran Unggulan
           </h1>
           <p className="opacity-70">
             Jelajahi Dunia Pengetahuan Melalui Pilihan Kami!
           </p>
-          <ul className="flex w-full overflow-x-auto gap-4 flex-nowrap scrollbar-hide no-scrollbar mt-5 mb-5">
-            <li className="whitespace-nowrap text-sm font-medium cursor-pointer hover:bg-gray-50 hover:border-gray-300 transition-colors">
-              Semua Kelas
-            </li>
-            <li className="whitespace-nowrap text-sm font-medium cursor-pointer hover:bg-gray-50 hover:border-gray-300 transition-colors">
-              Pemasaran
-            </li>
-            <li className="whitespace-nowrap text-sm font-medium cursor-pointer hover:bg-gray-50 hover:border-gray-300 transition-colors">
-              Desain
-            </li>
-            <li className="whitespace-nowrap text-sm font-medium cursor-pointer hover:bg-gray-50 hover:border-gray-300 transition-colors">
-              Pengembangan diri
-            </li>
-            <li className="whitespace-nowrap text-sm font-medium cursor-pointer hover:bg-gray-50 hover:border-gray-300 transition-colors">
-              Bisnis
-            </li>
+          <ul className="flex w-full overflow-x-auto gap-4  scrollbar-hide no-scrollbar mt-5 mb-5 ">
+            <li className="category-button">Semua Kelas</li>
+            <li className="category-button">Pemasaran</li>
+            <li className="category-button">Desain</li>
+            <li className="category-button">Pengembangan diri</li>
+            <li className="category-button">Bisnis</li>
           </ul>
           {/* card class section */}
           <section
-            className="grid gap-5 w-[94vw] mx-auto
+            className="grid gap-5 
           // responsive 
-           sm:grid-cols-3 lg:w-[88vw] lg:gap-3 xl:w-[80vw] xl:grid-cols-4 2xl:w-[80vw] 2xl:grid-cols-5"
+           sm:grid-cols-2 lg:gap-3 md:grid-cols-2 lg:grid-cols-3 xl:w-[96%] mx-auto"
           >
             {" "}
             {/* Added pb-10 for bottom spacing */}
             {loading ? (
-              <div className="col-span-full flex justify-center items-center py-20">
+              <div className="col-span-full flex-center py-20">
                 <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-green-500"></div>
               </div>
             ) : error ? (
@@ -106,6 +92,7 @@ function HomePage() {
                 {error}
               </div>
             ) : (
+              // display product
               products.map((product) => (
                 <Card
                   key={product.id}
@@ -123,14 +110,9 @@ function HomePage() {
           </section>
         </section>
         {/* newsletter section */}
-        <section className="relative text-white mx-auto top-95 mb-190 w-[94vw] lg:w-[90vw] xl:w-[80vw]">
-          <img
-            src={newsletter}
-            alt=""
-            loading="lazy"
-            className="rounded-md absolute inset-0 top-2 h-[335px] w-[94vw] lg:w-[90vw] xl:w-[80vw]"
-          />
-          <section className="overlay absolute top-2 text-center bg-black/80 rounded-md h-[335px] w-[94vw] lg:w-[90vw] xl:w-[80vw] flex justify-center items-center">
+        <section className="relative text-white mx-auto top-95 mb-190 responsive-width">
+          <div className="newsletter-bg rounded-md absolute top-2 h-[335px] w-full"></div>
+          <section className="overlay-style w-full">
             <section className="hero-text flex flex-col gap-2">
               <p>NEWSLETTER</p>
               <h1 className="text-[24px] font-bold ">
@@ -140,18 +122,23 @@ function HomePage() {
                 Daftarkan dirimu untuk mendapatkan informasi terbaru dan
                 penawaran spesial dari program-program terbaik harisenin.com
               </p>
-              <form action="" className=" flex gap-2 flex-col mx-auto relative">
+              <form
+                action=""
+                className=" flex gap-2 flex-col md:flex-row mx-auto relative"
+              >
                 <input
                   type="email"
                   placeholder="Masukkan Email"
                   className="bg-background w-[320px] text-center rounded-md text-black py-2 md:w-[450px] md:py-4 md:text-start md:px-5"
                 />
-                <button
+                <Button
                   type="submit"
-                  className="bg-yellow-500 w-[320px] text-center rounded-md text-background font-bold py-2 md:absolute md:w-[120px] md:top-2 md:right-2 "
+                  className="md:absolute md:w-[120px] md:top-2 md:right-2 "
+                  variant="yellow"
+                  size="lg"
                 >
                   Subscribe
-                </button>
+                </Button>
               </form>
             </section>
           </section>
